4add9288d6fbc0cbdeae6fdd423422c56db1a45e
nadia@mumuki.org <nadia@mumuki.org>
2023-03-22 19:45:18 +0000

    Mumuki Export on 2023-03-22 19:45:18 +0000

--- 00005_Double Combo/test.py
+++ 00005_Double Combo/test.py
@@ -1,24 +1,21 @@
 
-  def test_anterior1_es_0(self):
-    self.assertEqual(anterior(1), 0);
+  def test_previous_1_is_0(self):
+    self.assertEqual(previous(1), 0);
 
-  def test_anterior_10_es_9(self):
-    self.assertEqual(anterior(10), 9)
+  def test_previous_10_is_9(self):
+    self.assertEqual(previous(10), 9)
 
-  def test_triple_1_es_3(self):
+  def test_triple_1_is_3(self):
     self.assertEqual(triple(1), 3)
 
-  def test_triple_3_es_9(self):
+  def test_triple_3_is_9(self):
     self.assertEqual(triple(3), 9)
 
-  def test_anterior_del_triple_1_es_2(self):
-    self.assertEqual(anterior_del_triple(1), 2)
+  def test_previous_of_triple_1_is_2(self):
+    self.assertEqual(prev_of_triple(1), 2)
 
-  def test_anterior_del_triple_3_es_8(self):
-    self.assertEqual(anterior_del_triple(3), 8)
+  def test_previous_of_triple_3_is_8(self):
+    self.assertEqual(prev_of_triple(3), 8)
 
-  def test_anterior_del_triple_10_es_29(self):
-    self.assertEqual(anterior_del_triple(10), 29)
-
-
-
+  def test_previous_of_triple_10_is_29(self):
+    self.assertEqual(prev_of_triple(10), 29)
--- 00009_My favorite things/description.md
+++ 00009_My favorite things/description.md
@@ -1,3 +1,32 @@
+Let's define more functions! :writing_hand: Dani loves the first day of every month :date:, and that's why she defined this function...
+
+```python
+def is_favorite_day(day_of_month):
+  return day_of_month == 1
+```
+
+...and uses it like this (_and has left it in the library for you):
+
+```python
+ム is_favorite_day(13)
+False
+ム is_favorite_day(1)
+True
+```
+
+> Now it's your turn! Dani also says that someone `loves_reading`, when the number of books they remember reading is greater than 20. For example:
+>
+> ```python
+> ム loves_reading(15)
+> False
+>
+> ム loves_reading(45)
+> True
+> ```
+>
+> Define and test the function `loves_reading` in the console.
+
+
 ¡Definamos más funciones! :writing_hand: Dani ama el primer dia de cada mes :date:, y por eso definió esta función... 
 
 ```python
