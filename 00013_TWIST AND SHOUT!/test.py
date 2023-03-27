172a4db92fab4dbfcf73cd8eb3a562e3d19d66db
franco@mumuki.org <franco@mumuki.org>
2023-03-27 15:13:26 +0000

    Mumuki Export on 2023-03-27 15:13:26 +0000

--- 00013_TWIST AND SHOUT!/test.py
+++ 00013_TWIST AND SHOUT!/test.py
@@ -1,9 +1,9 @@
 
   def test_shout_miguel(self):
-    self.assertEqual(shout("miguel"), "MIGUEL!")
+    self.assertEqual(shout("shake it up, baby, now"), "SHAKE IT UP, BABY, NOW!")
 
   def test_shout_shout(self):
     self.assertEqual(shout("shout"), "SHOUT!")
 
   def test_shout_minuto(self):
-    self.assertEqual(shout("minuto"), "MINUTO!")
+    self.assertEqual(shout("twist"), "twist!")
